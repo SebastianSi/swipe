f55c029bcf2aa828183665932848b4dde271fc17
sebastian.sirb <sebastian.sirb90@gmail.com>
2018-04-27 15:46:55 +0300

    started from previous balls project and onto swipe proj

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#000'
     // alignItems: 'flex-start',
     // justifyContent: 'flex-start'
   },
